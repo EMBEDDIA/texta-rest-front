643bdf88cdbce978fe6c56113925fdf8ea66b344
Gert Paimla <gertpaimla@gmail.com>
2021-07-05 09:28:41 +0300

    fix

--- src/app/tools/topic-analyzer/view-cluster/view-cluster-documents/SignificantWordsWorker.ts
+++ src/app/tools/topic-analyzer/view-cluster/view-cluster-documents/SignificantWordsWorker.ts
@@ -1,4 +1,4 @@
 
 export class SignificantWordsWorker {
-  static worker = new Worker(new URL('./web-worker.worker', import.meta.url), {type: 'module'});
+  static worker = new Worker(new URL('./web-worker.worker', import .meta.url), {type: 'module'});
 }
