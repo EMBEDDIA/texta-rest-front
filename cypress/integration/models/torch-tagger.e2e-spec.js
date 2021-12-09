202a17631e3c44330b4e3cfceb315f300e190708
gpaimla <gertpaimla@gmail.com>
2021-12-09 21:02:12 +0200

    wip

--- cypress/integration/models/torch-tagger.e2e-spec.js
+++ cypress/integration/models/torch-tagger.e2e-spec.js
@@ -22,55 +22,6 @@ describe('Torch Taggers should work', function () {
     cy.get('mat-option').contains('integration_test_project').click();
   }
 
-  it('should be able to create a new tagger', function () {
-    cy.importTestEmbedding(this.projectId)
-    cy.wait('@importModel', {timeout: 50000})
-    initTorchTagger();
-    cy.get('[data-cy=appModelsTorchTaggerCreateBtn]').should('be.visible').click();
-    cy.get('[data-cy=appTorchTaggerCreateDialogDesc]').then((desc => {
-      cy.wrap(desc).should('have.class', 'mat-focused').type('b').find('input').clear();
-      cy.matFormFieldShouldHaveError(desc, 'required');
-      cy.wrap(desc).type('testTorch');
-    }));
-    cy.get('[data-cy=appTorchTaggerCreateDialogFields]').click().then((fields => {
-      cy.wrap(fields).should('have.class', 'mat-focused');
-      cy.closeCurrentCdkOverlay();
-      cy.matFormFieldShouldHaveError(fields, 'required');
-      cy.wrap(fields).click();
-      cy.get('.mat-option-text:nth(1)').should('be.visible').click();
-      cy.closeCurrentCdkOverlay();
-      cy.wrap(fields).find('mat-error').should('have.length', 0)
-    }));
-    cy.get('[data-cy=appTorchTaggerCreateDialogArchitecture]').click().then((architecture => {
-      cy.wrap(architecture).should('have.class', 'mat-focused');
-      cy.closeCurrentCdkOverlay();
-      cy.matFormFieldShouldHaveError(architecture, 'required');
-      cy.wrap(architecture).click();
-      cy.get('.mat-option-text:nth(1)').should('be.visible').click();
-      cy.closeCurrentCdkOverlay();
-      cy.wrap(architecture).find('mat-error').should('have.length', 0)
-    }));
-    cy.get('[data-cy=appTorchTaggerCreateDialogEmbedding]').click().then((embedding => {
-      cy.wrap(embedding).should('have.class', 'mat-focused');
-      cy.closeCurrentCdkOverlay();
-      cy.matFormFieldShouldHaveError(embedding, 'required');
-      cy.wrap(embedding).click();
-      cy.get('.mat-option-text:nth(1)').should('be.visible').click();
-      cy.closeCurrentCdkOverlay();
-      cy.wrap(embedding).find('mat-error').should('have.length', 0)
-    }));
-    cy.get('[data-cy=appTorchTaggerCreateDialogMaxSample]').click().then((sample => {
-      cy.wrap(sample).should('be.visible').find('input').clear();
-      cy.wrap(sample).type('10');
-    }));
-    cy.intercept('POST', '**/torchtaggers/**').as('postTorchTaggers');
-    cy.get('[data-cy=appTorchTaggerCreateDialogSubmit]').should('be.visible').click();
-    cy.wait('@postTorchTaggers').then(created => {
-      expect(created.response.statusCode).to.eq(201);
-      assert.equal(created.response.body.task.status, 'created');
-    });
-
-  });
   it('extra_actions should work', function () {
 
     cy.importTestTorchTagger(this.projectId)
@@ -124,6 +75,56 @@ describe('Torch Taggers should work', function () {
     cy.get('[data-cy=appTorchTaggerMenuDelete]').should('be.visible').click();
     cy.get('[data-cy=appConfirmDialogSubmit]').should('be.visible').click();
     cy.wait('@deleteTorchTaggers');
+  });
+  
+  it('should be able to create a new tagger', function () {
+    cy.importTestEmbedding(this.projectId)
+    cy.wait('@importModel', {timeout: 50000})
+    initTorchTagger();
+    cy.get('[data-cy=appModelsTorchTaggerCreateBtn]').should('be.visible').click();
+    cy.get('[data-cy=appTorchTaggerCreateDialogDesc]').then((desc => {
+      cy.wrap(desc).should('have.class', 'mat-focused').type('b').find('input').clear();
+      cy.matFormFieldShouldHaveError(desc, 'required');
+      cy.wrap(desc).type('testTorch');
+    }));
+    cy.get('[data-cy=appTorchTaggerCreateDialogFields]').click().then((fields => {
+      cy.wrap(fields).should('have.class', 'mat-focused');
+      cy.closeCurrentCdkOverlay();
+      cy.matFormFieldShouldHaveError(fields, 'required');
+      cy.wrap(fields).click();
+      cy.get('.mat-option-text:nth(1)').should('be.visible').click();
+      cy.closeCurrentCdkOverlay();
+      cy.wrap(fields).find('mat-error').should('have.length', 0)
+    }));
+    cy.get('[data-cy=appTorchTaggerCreateDialogArchitecture]').click().then((architecture => {
+      cy.wrap(architecture).should('have.class', 'mat-focused');
+      cy.closeCurrentCdkOverlay();
+      cy.matFormFieldShouldHaveError(architecture, 'required');
+      cy.wrap(architecture).click();
+      cy.get('.mat-option-text:nth(1)').should('be.visible').click();
+      cy.closeCurrentCdkOverlay();
+      cy.wrap(architecture).find('mat-error').should('have.length', 0)
+    }));
+    cy.get('[data-cy=appTorchTaggerCreateDialogEmbedding]').click().then((embedding => {
+      cy.wrap(embedding).should('have.class', 'mat-focused');
+      cy.closeCurrentCdkOverlay();
+      cy.matFormFieldShouldHaveError(embedding, 'required');
+      cy.wrap(embedding).click();
+      cy.get('.mat-option-text:nth(1)').should('be.visible').click();
+      cy.closeCurrentCdkOverlay();
+      cy.wrap(embedding).find('mat-error').should('have.length', 0)
+    }));
+    cy.get('[data-cy=appTorchTaggerCreateDialogMaxSample]').click().then((sample => {
+      cy.wrap(sample).should('be.visible').find('input').clear();
+      cy.wrap(sample).type('10');
+    }));
+    cy.intercept('POST', '**/torchtaggers/**').as('postTorchTaggers');
+    cy.get('[data-cy=appTorchTaggerCreateDialogSubmit]').should('be.visible').click();
+    cy.wait('@postTorchTaggers').then(created => {
+      expect(created.response.statusCode).to.eq(201);
+      assert.equal(created.response.body.task.status, 'created');
+    });
+
   });
 
 });
